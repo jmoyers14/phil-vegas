33337489c28beee3cd48280d19bb0891f7e8e1cc
Jeremy Moyers <jeremy@jeremymoyers.com>
2021-12-23 09:52:01 -0800

    clean bottom nav

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,54 +15,12 @@ const Footer = class extends React.Component {
                     <img
                         src={logo}
                         alt="Kaldi"
-                        style={{ width: '14em', height: '10em' }}
+                        style={{ width: '14em', height: '5em' }}
                     />
                 </div>
                 <div className="content has-text-centered has-background-black has-text-white-ter">
                     <div className="container has-background-black has-text-white-ter">
                         <div style={{ maxWidth: '100vw' }} className="columns">
-                            <div className="column is-4">
-                                <section className="menu">
-                                    <ul className="menu-list">
-                                        <li>
-                                            <Link
-                                                to="/"
-                                                className="navbar-item">
-                                                Home
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <a
-                                                className="navbar-item"
-                                                href="/admin/"
-                                                target="_blank"
-                                                rel="noopener noreferrer">
-                                                Admin
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </section>
-                            </div>
-                            <div className="column is-4">
-                                <section>
-                                    <ul className="menu-list">
-                                        <li>
-                                            <Link
-                                                className="navbar-item"
-                                                to="/blog">
-                                                Latest Stories
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link
-                                                className="navbar-item"
-                                                to="/contact">
-                                                Contact
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </section>
-                            </div>
                             <div className="column is-4 social">
                                 <a
                                     title="facebook"
@@ -92,6 +50,42 @@ const Footer = class extends React.Component {
                                         style={{ width: '1em', height: '1em' }}
                                     />
                                 </a>
+                            </div>
+                            <div className="column is-4">
+                                <section className="menu">
+                                    <ul className="menu-list">
+                                        <li>
+                                            <Link
+                                                to="/"
+                                                className="navbar-item">
+                                                Home
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link
+                                                className="navbar-item"
+                                                to="/blog">
+                                                Blog
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link
+                                                className="navbar-item"
+                                                to="/contact">
+                                                Contact
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <a
+                                                className="navbar-item"
+                                                href="/admin/"
+                                                target="_blank"
+                                                rel="noopener noreferrer">
+                                                Admin
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </section>
                             </div>
                         </div>
                     </div>
